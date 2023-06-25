9107eea036b776d215c5dd4a623cc176c7ae315a
Afeezope <afeezope1994@gmail.com>
2023-06-25 10:28:31 -0700

    Selection-sort files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,36 +1,45 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array using the selection algorithm
- * @array: pointer to array
- * @size: size of the array
- * Returns: void
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
 
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, selec_idx;
-	int selec_val;
+	int *min;
+	size_t i, j;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		selec_val = array[i];
-		selec_idx = i;
-		for (j = i; j < size; j++)
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
 		{
-			if (selec_val > array[j])
-			{
-				selec_idx = j;
-				selec_val = array[j];
-			}
+			swap_ints(array + i, min);
+			print_array(array, size);
 		}
-		if (selec_val == array[i] && selec_idx == i)
-			continue;
-		array[selec_idx] = array[i];
-		array[i] = selec_val;
-		print_array(array, size);
 	}
 }
